0eaa1bb8f0a188f9b784a26f550a7e7fb65046b0
Moe-95 <mhammad_fahs@hotmail.com>
2024-01-09 18:55:49 +0200

    9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -9,14 +9,16 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+size_t left_height = 0, right_height = 0;
+
 if (tree == NULL)
 return (0);
 
-size_t left_height = binary_tree_height(tree->left);
-size_t right_height = binary_tree_height(tree->right);
+if (tree->left != NULL)
+left_height = 1 + binary_tree_height(tree->left);
 
-if (left_height > right_height)
-return (1 + left_height);
-else
-return (1 + right_height);
+if (tree->right != NULL)
+right_height = 1 + binary_tree_height(tree->right);
+
+return ((left_height > right_height) ? left_height : right_height);
 }
