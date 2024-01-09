448c0960dc38a3dbac78135eac62fd837c06b1e6
Moe-95 <mhammad_fahs@hotmail.com>
2024-01-09 19:30:02 +0200

    fix 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -31,10 +31,7 @@ return ((left_height > right_height) ? left_height : right_height);
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-if (tree == NULL)
+if (tree)
+return (binary_tree_height(tree->left) - binary_tree_height(tree->right));
 return (0);
-
-int left_height = tree->left ? binary_tree_height(tree->left) + 1 : 0;
-int right_height = tree->right ? binary_tree_height(tree->right) + 1 : 0;
-return (left_height - right_height);
 }
